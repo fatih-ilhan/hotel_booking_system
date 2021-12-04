fe605164fca39335f5bca824d145e99094ac57e2
fi <fatihilhan66@gmail.com>
2021-12-04 16:53:42 -0500

    refinement

--- hotel/forms.py
+++ hotel/forms.py
@@ -17,9 +17,10 @@ class HotelCreateForm(ModelForm):
     def save(self, commit=True):
         hotel = super(ModelForm, self).save(commit=True)
         with connection.cursor() as cursor:
-            num_hotel = cursor.execute('SELECT COUNT(*) FROM hotel')
+            cursor.execute('SELECT COUNT(*) FROM hotel')
+            num_hotel = cursor.fethcone()[0]
             if num_hotel > 0:
-                cursor.execute('SELECT LAST_INSERT_ID() INTO @hotel;')  # TODO: if no row exists?
+                cursor.execute('SELECT LAST_INSERT_ID() INTO @hotel;')
                 hotel_id = cursor.fetchone()[0]
             else:
                 hotel_id = 0
@@ -62,10 +63,52 @@ class ReservationUpdateForm(ModelForm):
         fields = ['start_date', 'end_date']
 
     def save(self, commit=True):
-        # TODO: check the availability of new dates, assign new room if necessary
-        # room_list_ = filter_rooms(hotel_id, start_date, end_date, num_people, num_rooms)
+        res_id = self.instance.id
+        hotel_id = self.instance.hotel_id
 
-        reservation = super(ModelForm, self).save(commit=True)
+        start_date = self.cleaned_data['start_date']
+        end_date = self.cleaned_data['end_date']
+
+        old_start_date = self.instance.start_date
+        old_end_date = self.instance.end_date
+
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT room_id FROM reserved_room WHERE res_id = %s', [res_id])
+            room_id_list = cursor.fetchall()
+            cursor.execute('SELECT SUM(num_people) FROM room WHERE id IN %s', [room_id_list])
+            num_people = cursor.fetchone()[0]
+
+        num_rooms = len(room_id_list)
+
+        if end_date <= old_start_date or start_date >= old_end_date:
+            room_list = filter_rooms(hotel_id, start_date, end_date, num_people, num_rooms)
+            available_flag = all([r in [r_.id for r_ in room_list] for r in room_id_list])
+        else:
+            room_list_ = []
+            flag = False
+
+            if start_date < old_start_date:
+                room_list = filter_rooms(hotel_id, start_date, old_start_date, num_people, num_rooms)
+                room_list_.extend(room_list)
+                flag = True
+
+            if end_date > old_end_date:
+                room_list = filter_rooms(hotel_id, old_end_date, end_date, num_people, num_rooms)
+                room_list_.extend(room_list)
+                flag = True
+
+            room_list = list(set(room_list_))
+
+            if not flag:
+                available_flag = True
+            else:
+                available_flag = all([r in [r_.id for r_ in room_list] for r in room_id_list])
+
+        if available_flag:
+            reservation = super(ModelForm, self).save(commit=True)
+        else:
+            reservation = super(ModelForm, self).save(commit=False)
+
         return reservation
 
 
